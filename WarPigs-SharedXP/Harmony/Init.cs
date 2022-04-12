6006c22301b2b08322c564396ba2c3687731cbce
singlebarrel <cpkent@gmail.com>
2022-04-11 21:20:21 -0700

    The actual changes

---
+++ WarPigs-SharedXP/Harmony/Init.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace Harmony
+{
+    public class SharedXPMod : IModApi
+    {
+        public void InitMod(Mod _modInstance)
+        {
+            var harmony = new HarmonyLib.Harmony(GetType().ToString());
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
