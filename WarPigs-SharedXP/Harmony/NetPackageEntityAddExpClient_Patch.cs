93df9d193006c84beb1158f89e9b07f5a3cfc021
singlebarrel <cpkent@gmail.com>
2022-04-14 18:16:33 -0700

    Addming more logging

---
+++ WarPigs-SharedXP/Harmony/NetPackageEntityAddExpClient_Patch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarPigs.SharedXP.Harmony
+{
+    [HarmonyPatch(typeof(NetPackageEntityAddExpClient))]
+    [HarmonyPatch("ProcessPackage")]
+
+    public class NetPackageEntityAddExpClient_Patch
+    {
+        private static bool Prefix(NetPackageEntityAddExpClient __instance, World _world, GameManager _callbacks, int ___entityId, int ___xp, int ___xpType)
+        {
+            Log.Out($"AddExpClient> Entity: {___entityId} XP: {___xp} Type: {___xpType}");
+
+            // If I did not want the method we are patching to run at all, we would return false.
+            return true;
+        }
+    }
+}
---
+++ WarPigs-SharedXP/Harmony/NetPackageEntityAddExpServer_Patch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarPigs.SharedXP.Harmony
+{
+    [HarmonyPatch(typeof(NetPackageEntityAddExpServer))]
+    [HarmonyPatch("ProcessPackage")]
+
+    public class NetPackageEntityAddExpServer_Patch
+    {
+        private static bool Prefix(NetPackageEntityAddExpServer __instance, World _world, GameManager _callbacks, int ___entityId, int ___xp, int ___xpType)
+        {
+            Log.Out($"AddExpServer> Entity: {___entityId} XP: {___xp} Type: {___xpType}");
+
+            // If I did not want the method we are patching to run at all, we would return false.
+            return true;
+        }
+    }
+}
