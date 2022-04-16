5b890634deaf85ed80755d1bd6760001d9bb7480
singlebarrel <cpkent@gmail.com>
2022-04-16 16:57:13 -0700

    Adding logging

---
+++ WarPigs-SharedXP/Harmony/EntityBuff_SetCustomVar.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarPigs.SharedXP.Harmony
+{
+
+    [HarmonyPatch(typeof(EntityBuffs))]
+    [HarmonyPatch(nameof(EntityBuffs.SetCustomVar))]
+    public class EntityBuff_SetCustomVar
+    {
+        private static bool Prefix(EntityBuffs __instance, string _name, float _value, bool _netSync)
+        {
+            if (_name.StartsWith("_xp"))
+            {
+                Log.Out($"EntityBuff_SetCustomVar> {_name} {_value} {_netSync}");
+            }
+            return true;
+        }
+    }
+}
---
+++ WarPigs-SharedXP/Harmony/EntityBuff_SetCustomVarNetwork.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarPigs.SharedXP.Harmony
+{
+    [HarmonyPatch(typeof(EntityBuffs))]
+    [HarmonyPatch(nameof(EntityBuffs.SetCustomVarNetwork))]
+    public class EntityBuff_SetCustomVarNetwork
+    {
+        private static bool Prefix(EntityBuffs __instance, string _name, float _value)
+        {
+            if (_name.StartsWith("_xp"))
+            {
+                Log.Out($"EntityBuff_SetCustomVarNetwork> {_name} {_value}");
+            }
+            return true;
+        }
+
+    }
+}
