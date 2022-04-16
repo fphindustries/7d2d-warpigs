46bb929be9bd68f2e9499c329c49c5255b591c56
singlebarrel <cpkent@gmail.com>
2022-04-15 17:50:53 -0700

    Checking if we can see other players xp from client

--- WarPigs-SharedXP/Harmony/Progression_Patch.cs
+++ WarPigs-SharedXP/Harmony/Progression_Patch.cs
@@ -18,5 +18,27 @@ namespace WarPigs.SharedXP.Harmony
             // If I did not want the method we are patching to run at all, we would return false.
             return true;
         }
+
+        private static void Postfix(Progression __instance, int _exp, string _cvarXPName, Progression.XPTypes _xpType, bool useBonus)
+        {
+            foreach (EntityPlayer player in GameManager.Instance.World.Players.list)// this list is only of active players
+            {
+                //if (player != __instance.parent)
+                //{
+                    var playerXp = player.GetCVar(_cvarXPName);
+                    Log.Out($">> {player.GetDebugName()}: {_cvarXPName}: {playerXp}");
+                    //if (playerXp < maxExperience)
+                    //{
+                    //    var delta = maxExperience - playerXp;
+                    //    Log.Out($"Adding {delta} {_cvarXPName} xp to {player.EntityName}");
+
+                    //    NetPackageEntityAddExpClient package = NetPackageManager.GetPackage<NetPackageEntityAddExpClient>().Setup(player.entityId, Convert.ToInt32(delta), _xpType);
+                    //    SingletonMonoBehaviour<ConnectionManager>.Instance.SendPackage(package, false, player.entityId, -1, -1, -1);
+
+                    //    player.Progression.AddLevelExp(Convert.ToInt32(delta), _cvarXPName, _xpType, useBonus);
+                    //}
+                //}
+            }
+        }
     }
 }
