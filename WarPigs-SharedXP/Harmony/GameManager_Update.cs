78e1d15f32b7a8cce05f14ffdd8c3336d679a380
singlebarrel <cpkent@gmail.com>
2022-04-16 17:31:02 -0700

    derp

--- WarPigs-SharedXP/Harmony/GameManager_Update.cs
+++ WarPigs-SharedXP/Harmony/GameManager_Update.cs
@@ -19,7 +19,6 @@ namespace WarPigs.SharedXP.Harmony
 
         private static void Postfix(GameManager __instance)
         {
-            return;
             //Make sure the game is urnning
             if (!__instance.gameStateManager.IsGameStarted()) return;
 
